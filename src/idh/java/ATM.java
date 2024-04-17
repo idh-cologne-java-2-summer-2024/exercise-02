24b6b0cb1b4f28955c719b0116e4857e87bd96a9
ginkogruen <jasper@wolter-koeln.de>
2024-04-17 16:30:32 +0200

    Changed reference code to my own (slighly) different version.

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,17 +4,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
-
+	
 	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
+	 * Main command loop of the ATM
+	 * Asks the user to enter a number, and passes this number to the function cashout(...) 
+	 * which actually does the calculation and produces money.
+	 * If the user enters anything else than an integer number, the loop breaks 
+	 * and the program exists
 	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
+		while(true) {
 			try {
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
@@ -24,23 +24,24 @@ public class ATM {
 			}
 		}
 	}
-
+	
+	int cash = 200; // Amount of cash in posession of user
+	
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
+		if (amount > cash) {
 			System.out.println("Sorry, not enough money in the bank.");
+		} else {
+			cash = cash - amount;
+			System.out.println("Ok, here is you money, enjoy!");
+			System.out.println("New cash total: " + cash);
 		}
-
-	};
-
+	}
+	
 	/**
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
 		atm.run();
-	};
-
+	}
 }
