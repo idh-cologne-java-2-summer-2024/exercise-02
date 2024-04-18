3f4eda65a91c8e2cd2d73f083291f425448db644
Lilli <Lilli@LillisLaptop>
2024-04-18 14:30:25 +0200

    Änderungen: 1.Der Automat sollte verschiedene Konten verwalten können. D.h. dass Benutzer:innen im Dialog zunächst ihre Kontonummer angeben müssen, und danach den Betrag. Der Geldautomat weiß dann für alle inviduellen Benutzer:innen, wie viel Geld diese noch auf dem Konto haben. 2.Der Automat verwaltet außerdem, wie viel Bargeld er noch zur Verfügung hat. D.h. auch wenn eine Benutzerin genug Geld auf dem Konto hat, kann es sein, dass sie das Geld nicht bekommt, weil der Automat leer ist. Wenn eine Auszahlung passiert ist, sollte der Automat seinen Bargeldbestand jeweils aktualisieren.

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -5,13 +5,35 @@ import java.io.InputStreamReader;
 
 public class ATM {
 	int accountBalance = 100;
-
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
+	
+	public void start () {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			try {
+				System.out.print("Enter your account number: ");
+				int accountNumber = Integer.parseInt(br.readLine());
+				account(accountNumber);
+				BufferedReader bu = new BufferedReader(new InputStreamReader(System.in));
+				while (true) {
+					try {
+						System.out.print("Enter the amount to withdraw: ");
+						int amount = Integer.parseInt(bu.readLine());
+						cashout(amount);
+					} catch (Exception e) {
+						break;
+					}
+					}
+			} catch (Exception e) {
+				break;
+			}
+		}
+	}
+	public void account (int accountNumber) {
+		if (accountNumber == 123 || accountNumber == 234 || accountNumber == 345 || accountNumber == 456) {
+		}else {
+			System.out.println("This account number doesn't exist.");
+			}}
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -22,24 +44,26 @@ public class ATM {
 			} catch (Exception e) {
 				break;
 			}
+			}
 		}
+	public void cashout(int amount) {
+			if (amount < accountBalance) {
+				accountBalance = accountBalance - amount;
+				System.out.println("Ok, here is your money, enjoy!");
+			} else {
+				System.out.println("Sorry, not enough money in the bank.");
+			}
+			
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
-		}
-
-	};
+	
 
 	/**
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
+		atm.start();
 		atm.run();
 	};
 
