122b66cdef4e4ae997bce85df678bbb325f4992c
luise <luise@AcerSwitch-LF>
2024-04-18 19:16:03 +0200

    added useraccounts and functions to check if useraccounts exists, have money and if the atm itself has enough money to withdraw.

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -5,42 +5,77 @@ import java.io.InputStreamReader;
 
 public class ATM {
 	int accountBalance = 100;
-
+	int atmBalance= 50000; 
+	userAccount[] userAccounts = new userAccount [4];
+	public ATM( ) {
+		
+		userAccounts[0]=  new userAccount(1234, 5000, "Bibi"); 
+		userAccounts[1] = new userAccount(9876, 20000, "Tina"); 
+		userAccounts[2] = new userAccount(7654, 50, "Amadeus"); 
+		userAccounts[3] = new userAccount(4321, 966745656, "Sabrina");
+		
+	}
+	
 	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
+	 * Main command loop of the ATM Asks the user to enter a number, and checks with the function checkAccountNbr 
+	 * if the number is an existing account number and returns the array-slot of the account as an integer, if it exists. 
+	 * If the number is an existing Account number it asks for an Amount to withdraw and passes the new number and the array-slot
+	 *-number to the function cashout(...) which actually does the calculation and checks if the atm has enough money and the balance of the account owner is high enough.
+	 * If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
 	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
-			try {System.out.print("Welcome! Please enter your accountnumber: ");
+			try {
+				System.out.print("Welcome! Please enter your accountnumber: ");
 			int accountNbr = Integer.parseInt(br.readLine());
-				
-			 if (existingAccount(accountNbr){
-				 System.out.print("Enter the amount to withdraw: ");
+			int account = checkAccountNbr(accountNbr);
+			if (account>=0) {
+				 System.out.print("Hello "+ userAccounts[account].userName + "! Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
-			 } 
-			 else {
-				 System.out.print("This Account does not exist. Please try again");
+				cashout(account, amount);
+			 } else {
+					 System.out.println("This Account does not exist. Please try again");
 			 }
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
-	public boolean existingAccount(int accountNbr) {
+	
+	/* checks if accountnumber belongs to an existing account by looping through an array with useraccounts-objects
+	*and compares the accountnumbers. If true, it returns the array-slot of the useraccount. Else it returns an 
+    *array-slot.  
+	*/
+	public int checkAccountNbr(int accountNbr) {
 		
-		return accountExists
+		for (int i=0; i<=userAccounts.length; i++) {
+			
+			if (userAccounts[i].accountNumber==accountNbr) {
+				return i; 
+			}
+		}
+		return -1;
 	}
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
+	/*takes the array-slot of the account and the wanted amount and checks if the amount is smaller then the cash-reserve
+	 * of the atm. If it is it checks if the user has enough money, using the array-slot of the account. If true, the money will be withdrawn 
+	 * and the balance of the atm and the account will be updated.  
+	 * No money will be withdrawn if one or both checks fail. 
+	 */
+	public void cashout(int account, int amount) {
+		
+		if (amount <= atmBalance) {
+			if (amount <=userAccounts[account].userBalance) {
+			
+				userAccounts[account].userBalance = userAccounts[account].userBalance - amount;
+				this.atmBalance= this.atmBalance-amount; 
+				System.out.println("Ok, here is your money, enjoy! Your new Balance is: "+userAccounts[account].userBalance+"€");
+			} else {
+				System.out.println("Sorry, you're broke");
+			}
 		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			System.out.println("Sorry, not enough money in this ATM.");
 		}
 
 	};
@@ -49,8 +84,10 @@ public class ATM {
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
+		
+		
 		ATM atm = new ATM();
 		atm.run();
-	};
+	}
 
 }
---
+++ src/idh/java/userAccount.java
@@ -0,0 +1,23 @@
+package idh.java;
+
+public class userAccount {
+	/* class in which userAccounts as objects with variables are defined and with a constructor to assign values to these variables. AserAccounts need
+	 * an accountNumber, user Balance and a userName.
+	 */
+ int accountNumber;
+ int userBalance; 
+ String userName; 
+ 
+ public userAccount(int number, int balance, String name) {
+	 this.accountNumber=number; 
+	 this.userBalance=balance; 
+	 this.userName=name;
+	 
+ }
+ 
+ public static void main(String[] args) {
+		
+		
+	}
+}
+
