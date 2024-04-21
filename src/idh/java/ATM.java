bc61d59d83703e3ad0541d4303ac8ca496ec8424
marvi <marvi@DESKTOP-37RQBMC.fritz.box>
2024-04-21 22:49:57 +0200

    FertigerVersuchExercise02

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,46 +2,62 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ATM {
-	int accountBalance = 100;
+    private Map<String, Integer> accountBalances = new HashMap<>(); // accounts with balances
+    private int cashInATM = 500; // starting balance
 
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
-	// Teständerung fürs committen
-	public void run() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			try {
-				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
-			} catch (Exception e) {
-				break;
-			}
-		}
-	}
+    /**
+     * main command loop of the ATM. Asks the user for an account number & amount money, then provides cash
+     */
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.print("Enter your account number: ");
+                String accountNumber = br.readLine();
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
-		}
+                // if account number not available, it gets added
+                if (!accountBalances.containsKey(accountNumber)) {
+                    accountBalances.put(accountNumber, 500); // starting balance 500
+                    System.out.println("New account created with initial balance of 500.");
+                }
 
-	};
+                System.out.print("Enter the amount to withdraw: ");
+                int amount = Integer.parseInt(br.readLine());
+                cashout(accountNumber, amount);
 
-	/**
-	 * Launches the ATM
-	 */
-	public static void main(String[] args) {
-		ATM atm = new ATM();
-		atm.run();
-	};
+            } catch (Exception e) {
+                System.out.println("Wrong input moron, try again!");
+                break;
+            }
+        }
+    }
 
+    /**
+     * function for withdrawing money. Checks if user has enough money and if the ATM has enough cash
+     */
+    public void cashout(String accountNumber, int amount) {
+        if (amount <= accountBalances.get(accountNumber)) {
+            if (amount <= cashInATM) {
+                accountBalances.put(accountNumber, accountBalances.get(accountNumber) - amount);
+                cashInATM -= amount;
+                System.out.println("Ok, here you go!");
+            } else {
+                System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
+            }
+        } else {
+            System.out.println("Sorry, you don't have enough money in the bank.");
+        }
+    }
+
+    /**
+     * starts the ATM.
+     */
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.run();
+    }
 }
