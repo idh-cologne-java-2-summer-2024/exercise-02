c79c17e9f658d5dc822542869e37f77ede4a651f
Test <Test@LAPTOP-B2MDAQN6.fritz.box>
2024-04-23 21:56:14 +0200

    Konstruktor für verschiedene Konten erstellt

--- src/idh/java/Account.java
+++ src/idh/java/Account.java
@@ -1,5 +1,11 @@
 package idh.java;
 
 public class Account {
-
+	int accountnumber;
+	int individualbalance;
+	
+public Account(int accountnumber, int individualbalance) {
+	this.accountnumber = accountnumber;
+	this.individualbalance = individualbalance;
+}
 }
