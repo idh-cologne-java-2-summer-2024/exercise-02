54ebd432244ec5fbb91b740c695557b5c3cbded5
oona- <oona-@192.168.178.156>
2024-04-23 23:18:40 +0200

    Aufgabe 2 oona Yu-Mi Song

---
+++ src/idh/java/Bankkonto.java
@@ -0,0 +1,15 @@
+package idh.java;
+
+public class Bankkonto {
+int kontonummer;
+String kontoinhaber;
+int guthaben;
+	
+
+public Bankkonto(int kontonummer, String kontoinhaber, int guthaben) {
+	super();
+	this.kontonummer = kontonummer;
+	this.kontoinhaber = kontoinhaber;
+	this.guthaben = guthaben;
+}
+}
