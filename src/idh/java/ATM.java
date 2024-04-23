35989ea5270ada774f2912b8ddabd460607ecd95
loki <loki@192.168.8.112>
2024-04-23 16:28:19 +0200

    java HA2

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,40 +1,92 @@
 package idh.java;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class ATM {
-	int accountBalance = 100;
-
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
 	 * produces money. If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
 	 */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ATM {
+int accountBalance = 100;
+	public class Bankkonto {
+		int kontonummer;
+		String kontoinhaber;
+		int guthaben; 
+		
+	public Bankkonto(int kontonummer, String kontoinhaber, int guthaben) {
+		this.kontonummer= kontonummer;
+		this.kontoinhaber = kontoinhaber;
+		this.guthaben = guthaben;
+	}
+		
+	}
+	int moneyDepot = 400;
+	
+	Bankkonto[] kontos; 
+	
+	public ATM () {
+		System.out.println( "ATM wird gestartet..");
+		kontos = new Bankkonto[10];
+		kontos[0] = new Bankkonto (281," taya", 100);
+		
+	}
+	
+ 
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.print("Enter your Iban: ");
+				int iban =Integer.parseInt(br.readLine());
+				
+				
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount,iban);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+	public void cashout(int amount, int iban) {
+		
+		Bankkonto konto = getBankkontowithIban(iban);
+		
+		if (konto== null) {
+			return;
+			
+		}
+		if (konto.guthaben < amount) {
+			System.out.println("Sorry, your dont have enough money in the bank. ");
 		}
 
+		if ((moneyDepot - amount) >= 0) {
+			moneyDepot = moneyDepot - amount;
+			konto.guthaben=konto.guthaben - amount;
+			System.out.println("Ok, here is your money enjoy!");
+		} else {
+			System.out.println("Sorry not enough money in the bank");
+		}
 	};
 
+	
+	private Bankkonto getBankkontowithIban (int iban) {
+		for (Bankkonto konto : kontos) {
+			if (konto != null) {
+				System.out.println(" No account!");
+				if (konto.kontonummer== iban) {
+					return konto; 
+				}
+	}
+		}
+		return null;
+	}
 	/**
 	 * Launches the ATM
 	 */
@@ -42,5 +94,4 @@ public class ATM {
 		ATM atm = new ATM();
 		atm.run();
 	};
-
-}
+};
