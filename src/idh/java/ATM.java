e5af1a81f58c46b5d88e14256f9287b8ca06274c
oona- <oona-@192.168.178.156>
2024-04-23 23:02:56 +0200

    java aufgabe 2 oona yu-mi song

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -6,26 +6,53 @@ import java.io.InputStreamReader;
 public class ATM {
 	int accountBalance = 100;
 
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
+	
+	public ATM() {
+		
+		
+		
+		System.out.println("Karte Einfügen...");
+		System.out.println(   );
+		System.out.println("[Kontostand einsehen]    [Bargeld einzahlen]");
+		System.out.println("[Abbruch]                [Bargeld auszahlen]");
+		System.out.println(   );
+	 	
+		Bankkonto[] kontos = new Bankkonto[10];
+		 
+		 kontos[0]= new Bankkonto(034, "Wilhelm", 100);
+		 kontos[1]= new Bankkonto(7368, "Keanu", 60);
+	}
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
+				
+				System.out.println("Kontonummer eingeben:");
+				int accountNumber= Integer.parseInt(br.readLine());
+				
+				
+				System.out.print("Gib den Auszahlungsbetrag ein: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount, accountNumber);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
+	public void cashout(int amount, int accountNumber) {
+		
+		Bankkonto konto = getBankkontoByAccountNumber(accountNumber);
+		
+		if (konto== null) {
+			return;
+		}
+		
+		if (konto.guthaben < amount) {
+			System.out.println("Sorry, you are too broke...");
+			return;
+		}
 		if (amount < accountBalance) {
 			accountBalance = accountBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
@@ -34,7 +61,21 @@ public class ATM {
 		}
 
 	};
+	private Bankkonto getBankkontoByAccountNumber(int accountNumber) {
+	
 
+	
+	for (Bankkonto konto: kontos) {
+ 		
+ 		if ( konto != null) {
+ 			if (konto.kontonummer== accountNumber) {
+ 				
+ 			}
+ 		}
+ 	}
+ 	return null;
+ 		
+ 	}
 	/**
 	 * Launches the ATM
 	 */
