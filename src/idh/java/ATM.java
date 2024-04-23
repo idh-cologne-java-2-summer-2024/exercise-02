262c3eb87066f8f01d827c9682d0437e69111e52
Marc Tetsuo Benscheid <mbensche@smail.de>
2024-04-23 12:40:17 +0200

    created new User Class as a POJO

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,18 +4,21 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 //i changed something 
 public class ATM {
-	int accountBalance = 100;
-
+	int accountBalance = 300;
+	
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
 	 * produces money. If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
 	 */
-	public void run() {
+	public void run(User[] u) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.println("Enter your name.");
+			
+				
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(amount);
@@ -40,7 +43,13 @@ public class ATM {
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
-		atm.run();
+		User[] users = new User[20];
+		User u1 = new User(1, 50, "Max Muster");
+		User u2 = new User(2, 100, "Timmy Turner");
+		users[0] = u1;
+		users[1] = u2;
+		System.out.println(users[0].name);
+//		atm.run(users);
 	};
 
 }
