8adb5a8856ad340f818565d2ef390884cb6ff674
dejag <dejag@172.29.235.13>
2024-04-23 21:31:41 +0200

    Erfüllung der Hausaufgabe JavaII für bis zum 24.April

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,43 +4,49 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
+	private int accountBalance = 100;
+	Bankkonto[] kontos;
 
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
+	public ATM() {
+		System.out.println("ATM wird gestartet...");
+		kontos = new Bankkonto[20];
+		kontos[0] = new Bankkonto(1234, "Nils", 100);
+		kontos[1] = new Bankkonto(5678, "Max Mustermann", 150);}
+
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
+				System.out.print("Enter your account number: ");
+				int accountNumber = Integer.parseInt(br.readLine());
+				System.out.println("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount, accountNumber);
 			} catch (Exception e) {
-				break;
-			}
-		}
-	}
+				break;}}}
 
-	public void cashout(int amount) {
+	public void cashout(int amount, int accountNumber) {
+		Bankkonto konto =  getBankontoByAccountNumber(accountNumber);
+		if(konto == null) {
+			System.out.println("Account with account number "+ accountNumber +" not found");
+			return;}
+		if(konto.guthaben < amount) {
+			System.out.println("Sorry, you don't have enough money in the bank.");
+			return;}
 		if (amount < accountBalance) {
 			accountBalance = accountBalance - amount;
+			konto.guthaben = konto.guthaben - amount;
 			System.out.println("Ok, here is your money, enjoy!");
 		} else {
-			System.out.println("Sorry, not enough money in the bank.");
-		}
+			System.out.println("Sorry, not enough money in your account.");}};
 
-	};
-
-	/**
-	 * Launches the ATM
-	 */
+	private Bankkonto getBankontoByAccountNumber(int accountNumber) {
+		for(Bankkonto konto : kontos) {
+			if(konto != null) {	
+				if(konto.kontonummer == accountNumber) {
+					return konto;}}}
+			return null;}
+	
 	public static void main(String[] args) {
 		ATM atm = new ATM();
-		atm.run();
-	};
-
-}
+		atm.run();};}
---
+++ src/idh/java/Bankkonto.java
@@ -0,0 +1,11 @@
+package idh.java;
+
+public class Bankkonto {
+		int kontonummer;
+		String kontoinhaber;
+		int guthaben;
+		public Bankkonto(int kontonummer, String kontoinhaber, int guthaben) {
+			super();
+			this.kontonummer = kontonummer;
+			this.kontoinhaber = kontoinhaber;
+			this.guthaben = guthaben;}}
