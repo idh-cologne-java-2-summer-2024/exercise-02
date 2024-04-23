4e7861794173c96358ab3271cd9455a8c3531c75
leoni <leoni@Leonies_Laptop.fritz.box>
2024-04-23 20:32:52 +0200

    fixed class

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,7 +4,16 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int kontostand= 100;
+	private int geldInBank= 100;
+	
+	bankkonto[] kontos;
+	
+	public ATM() {
+		System.out.println("Atm wir gestarted...");
+		
+		kontos = new bankkonto [20];
+		kontos[0] = new bankkonto(1234,"leonie",50);
+	}
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -16,10 +25,14 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.print("Enter the Accountnumber: ");
+				int accountNUmber = Integer.parseInt(br.readLine());
+				
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
-				if (kontostand < amount) {
+				cashout(amount,accountNUmber);
+				
+				if (geldInBank < amount) {
                     System.out.print("Enter the amount to withdraw: exit ");
                     break;}
 			} catch (Exception e) {
@@ -28,15 +41,41 @@ public class ATM {
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < kontostand) {
-			kontostand = kontostand - amount;
+	public void cashout(int amount, int accountNumber) {
+		
+		bankkonto konto = getBankkontoByAccountNumber(accountNumber);
+		
+		if(konto==null) {
+			System.out.println("Account with account number"+ accountNumber + "not found");
+			return;
+		}
+		
+		if (konto.guthaben < amount) {
+			System.out.println("Sorry, not enough money in your account.");
+			return;
+		}
+		
+		
+		if (amount <= geldInBank) {
+			geldInBank = geldInBank- amount;
+			konto.guthaben = konto.guthaben - amount;
 			System.out.println("Ok, here is your money, enjoy!");
 		} else {
 			System.out.println("Sorry, not enough money in the bank.");
 		}
 
 	};
+	private bankkonto getBankkontoByAccountNumber (int accountNumber) {
+		
+		for (bankkonto konto : kontos) {
+			if (konto != null) {
+				if (konto.kontonummer == accountNumber) {
+					return konto;
+				}
+			}
+		}
+		return null;
+	}
 
 	/**
 	 * Launches the ATM
---
+++ src/idh/java/bankkonto.java
@@ -0,0 +1,17 @@
+package idh.java;
+
+public class bankkonto {
+	int kontonummer; 
+    String kontoinhaber;
+    int guthaben;
+
+    public bankkonto(int kontonummer, String kontoinhaber, int guthaben) {
+        super();
+        this.kontonummer = kontonummer;
+        this.kontoinhaber = kontoinhaber;
+        this.guthaben = guthaben;
+    }
+    
+}
+
+
