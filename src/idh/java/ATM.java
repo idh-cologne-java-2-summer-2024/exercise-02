40dfbe32c662f413db7054e0232ae4114dd38b53
pauls <pauls@PsPC.fritz.box>
2024-04-23 10:55:42 +0200

    Update

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,10 +4,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
-
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
+	
+	int cash = 200;
+	Bankkonto[] kontos;
+			
+	public ATM() {
+		System.out.println("ARM wird gestartet!");
+		kontos = new Bankkonto[20];
+		kontos[0] = new Bankkonto(0,"Uwe", 200);
+		kontos[1] = new Bankkonto(1,"Sabrina", 200);
+		kontos[2] = new Bankkonto(2,"Godfried", 200);
+		kontos[3] = new Bankkonto(3,"Agatha", 200);
+	}
+	
+			/**
 	 * number to the function cashout(...) which actually does the calculation and
 	 * produces money. If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
@@ -16,25 +26,46 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
+				System.out.print("Enter your account number: ");
+				int accountnumber = Integer.parseInt(br.readLine());
+				
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount, accountnumber);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
-
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+	
+	public void cashout(int amount, int accountnumber) {
+		
+		Bankkonto konto = getKontobyAccountnumber(accountnumber);
+		
+		if (amount < konto.guthaben && cash >= amount) {
+			konto.guthaben = konto.guthaben - amount;
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+		}
+		else {
+			if(amount > konto.guthaben) {
+				System.out.println("Sorry, not enough money in the bank.");
+			} else {
+			if(cash < amount) {
+				System.out.println("Sorry, the ATM doesn't have that much cash anymore");
+			}}
+		}
 		}
 
-	};
-
+	private Bankkonto getKontobyAccountnumber(int accountnumber) {
+		for(Bankkonto konto : kontos) {
+			if(konto != null && konto.kontonummer == accountnumber) {
+					System.out.print("Enter the amount to withdraw: ");
+					return konto;
+				
+			}
+		}
+		System.out.println("Es wurde kein Konto gefunden!");
+		return null;
+	}
 	/**
 	 * Launches the ATM
 	 */
