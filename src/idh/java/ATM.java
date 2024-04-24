85fb10a8c5453c05cd3e455f9307ceed6936cd84
User <User@DESKTOP-I1TSJLG.speedport.ip>
2024-04-24 19:14:48 +0200

    Excercise 02 - Erweiterung der ATM-Klasse

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -5,6 +5,14 @@ import java.io.InputStreamReader;
 
 public class ATM {
 	int accountBalance = 100;
+	ATMaccount [] accounts;
+	
+	public ATM() {
+		accounts = new ATMaccount[10];
+		accounts[0] = new ATMaccount(100, "123");
+		accounts[1] = new ATMaccount(400, "234");
+		accounts[2] = new ATMaccount(100, "345");
+	}
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -12,22 +20,47 @@ public class ATM {
 	 * produces money. If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
 	 */
+	
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
-			try {
-				System.out.print("Enter the amount to withdraw: ");
+		try {
+			System.out.print("Enter your account number: ");
+			String accountNumber = br.readLine();
+			
+			System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount, accountNumber);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
+	
+	public ATMaccount getATMaccountByaccountNumber (String accountNumber) {
+		for (ATMaccount account : accounts) {
+			if(account != null) {
+				System.out.println("Ok, here you go!");
+			} else { 
+				System.out.println("Sorry, you don't have enough money in the bank");
+			
+			}
+		}
+		return null;
+		
+	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
+	public void cashout( int amount, String accountNumber) {
+		ATMaccount account = getATMaccountByaccountNumber(accountNumber);
+		
+		if (account ==null) {
+			return;
+		}
+		
+		if (amount <= accountBalance) {
 			accountBalance = accountBalance - amount;
+			
 			System.out.println("Ok, here is your money, enjoy!");
 		} else {
 			System.out.println("Sorry, not enough money in the bank.");
@@ -39,8 +72,9 @@ public class ATM {
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
-		ATM atm = new ATM();
+		ATM atm = new ATM(); 
 		atm.run();
+	
 	};
 
 }
