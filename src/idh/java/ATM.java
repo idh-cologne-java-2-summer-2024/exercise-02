0bf51e508a4c6dad676d2e37cf86c521131d80b5
DaveSanti <dgolomb94@gmail.com>
2024-06-11 14:51:37 +0200

    Vergessen ins Stage Change zu verschieben :'(

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,43 +4,54 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
+    int[] accountNumbers = {123, 234, 345};
+    int[] accountBalances = {200, 1000, 500};
+    int cashInATM = 500;
 
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
-	public void run() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			try {
-				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
-			} catch (Exception e) {
-				break;
-			}
-		}
-	}
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.print("Enter your account number: ");
+                int accountNumber = Integer.parseInt(br.readLine());
+                System.out.print("Enter the amount to withdraw: ");
+                int amount = Integer.parseInt(br.readLine());
+                cashout(accountNumber, amount);
+            } catch (Exception e) {
+                break;
+            }
+        }
+    }
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
-		}
+    public void cashout(int accountNumber, int amount) {
+        int index = -1;
+        for (int i = 0; i < accountNumbers.length; i++) {
+            if (accountNumbers[i] == accountNumber) {
+                index = i;
+                break;
+            }
+        }
 
-	};
+        if (index != -1) {
+            int accountBalance = accountBalances[index];
+            if (amount <= accountBalance) {
+                if (amount <= cashInATM) {
+                    accountBalances[index] -= amount;
+                    cashInATM -= amount;
+                    System.out.println("Ok, here you go!");
+                } else {
+                    System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
+                }
+            } else {
+                System.out.println("Sorry, you don't have enough money in the bank.");
+            }
+        } else {
+            System.out.println("Account number not found.");
+        }
+    }
 
-	/**
-	 * Launches the ATM
-	 */
-	public static void main(String[] args) {
-		ATM atm = new ATM();
-		atm.run();
-	};
-
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.run();
+    }
 }
